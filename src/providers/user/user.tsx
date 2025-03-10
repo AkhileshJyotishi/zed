32dbe759560ad49e7bf28a298e2fc7079662f530
AkhileshJyotishi <akhileshjyotishi1729@gmail.com>
2025-03-10 17:05:35 +0530

    user provider added

---
+++ src/providers/user/user.tsx
@@ -0,0 +1,32 @@
+"use client"
+import React, { useState } from "react"
+
+interface IUserContext {
+  user: ITemporaryVariable | null
+}
+
+type IUserProvider = IProvider
+
+const defaultValues: IUserContext = {
+  user: null,
+}
+
+const Context = React.createContext<IUserContext>(defaultValues)
+
+const UserProvider: React.FC<IUserProvider> = ({ children }) => {
+  const [user] = useState<ITemporaryVariable | null>(null)
+
+  return <Context.Provider value={{ user }}>{children}</Context.Provider>
+}
+
+const useUser = () => {
+  const c = React.useContext(Context)
+
+  if (c === undefined) {
+    throw new Error("useUser must be used within a UserProvider")
+  }
+
+  return c
+}
+
+export { UserProvider, useUser }
