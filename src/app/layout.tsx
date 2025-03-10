622cd633e164033d01c1d00749eb55afabdfdeab
Ankit Yadav <mahesh9426346889@gmail.com>
2025-03-10 17:29:32 +0530

    Add homepage

---
+++ src/app/layout.tsx
@@ -0,0 +1,68 @@
+import React from "react"
+
+import { Metadata } from "next"
+
+import { Toaster } from "sonner"
+
+import { navItems } from "@/data/sample"
+
+import Navbar from "@/components/navbar"
+import { Provider } from "@/providers"
+import { FloatingNav } from "@/ui/floating-navbar"
+
+import "@/styles/globals.css"
+
+export const metadata: Metadata = {
+  title: {
+    default: "Zed",
+    template: "Zed | %s",
+  },
+  description: "",
+  verification: {
+    me: "",
+    google: "",
+    yahoo: "",
+  },
+}
+const links: { label: string; href: string }[] = [
+  {
+    label: "Home",
+    href: "/dashboard",
+  },
+  {
+    label: "About Us",
+    href: "/about",
+  },
+  {
+    label: "IDE",
+    href: "/ide",
+  },
+]
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) => {
+  return (
+    <html lang="en">
+      <body className="min-h-screen bg-secondary px-2.5 font-satoshi text-light-0 sm:px-5">
+        <Provider>
+          <Toaster
+            position="top-center"
+            invert
+          />
+          <FloatingNav navItems={navItems} />
+          <Navbar
+            links={links}
+            logoText="Zed"
+            textColor="white"
+          />
+          <>{children}</>
+        </Provider>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
